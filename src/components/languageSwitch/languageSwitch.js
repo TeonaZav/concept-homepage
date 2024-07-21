7fe02fb7866ba4f5c62b2f6d55e71f465ed0fbf2
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-07-21 23:38:56 +0400

    add content and styles to languageSwitch.js

--- src/components/languageSwitch/languageSwitch.js
+++ src/components/languageSwitch/languageSwitch.js
@@ -1,1 +1,27 @@
-export const languageSwitch = () => {};
+import styles from "./languageSwitch.module.scss";
+import { getLanguageIcon } from "../../utils/icons";
+
+export const languageSwitch = (currentLang = "ge") => {
+  const langText = currentLang === "en" ? "Eng" : "ქარ";
+  const otherLang = currentLang === "en" ? "ქარ" : "Eng";
+  const otherLangCode = currentLang === "en" ? "ge" : "en";
+
+  return `
+      <div class="${styles.language}">
+        <div class="${styles.languageSwitch}">
+          ${getLanguageIcon()} 
+          <span class="${
+            styles.languageText
+          }" data-current-lang="${currentLang}">
+            ${langText}
+          </span>
+        </div>
+        <div class="${styles.languageDropdown}">
+          <button class="${styles.languageOption}" data-lang="${otherLangCode}">
+            ${otherLang}
+          </button>
+        </div>
+      </div>
+    `;
+};
+
