02d3c3a0e4d9eaaf942aabcdd1fc2efe61bf64ae
Teona Zavrashvili <teona.zavrashvili@gmail.com>
2024-07-25 01:24:19 +0400

    add text section

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -4,7 +4,7 @@ import { Hero } from "../../components/hero/hero";
 import { DigitalBankingSection } from "../../components/digitalBanking/digitalBankingSection";
 import { StatsSection } from "../../components/statsSection/statsSection";
 import { SelectPackageSection } from "../../components/selectPackage/selectPackage";
-
+import { TextSection } from "../../components/textSection/textSection";
 export class Home {
   render() {
     const headerElement = document.getElementById("header");
@@ -17,7 +17,10 @@ export class Home {
     });
     const hero = new Hero();
     hero.render(mainElement);
-    
+
+    const textSection = new TextSection();
+    textSection.render(mainElement);
+
     const selectPackageSection = new SelectPackageSection();
     selectPackageSection.render(mainElement);
 
